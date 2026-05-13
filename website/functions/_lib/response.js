c37d7e3c157a1a60652eb3ac1f56571655beaafb
deutscheassy <deutschtony2026@gmail.com>
2026-05-14 00:07:29 +0700

    renew

--- website/functions/_lib/response.js
+++
@@ -1,9 +0,0 @@
-export function json(data, init = {}) {
-  const headers = new Headers(init.headers || {});
-  headers.set("Content-Type", "application/json; charset=utf-8");
-
-  return new Response(JSON.stringify(data), {
-    ...init,
-    headers
-  });
-}
--- website/functions/api/vocab/meta.js
+++
@@ -1,9 +0,0 @@
-import { getVocabMeta } from "../../_lib/data.js";
-import { json } from "../../_lib/response.js";
-
-export function onRequestGet() {
-  return json({
-    ok: true,
-    ...getVocabMeta()
-  });
-}
